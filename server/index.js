fcd794101be3bda49e71ba18c2518c64df68bea8
Funya1337 <funya7654@gmail.com>
2020-01-03 13:55:00 +0300

    refactor(server): rewrite to async await

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Bcrypt = require("bcryptjs");
 const express = require("express");
 const bodyParser = require("body-parser");
 let userSchema = require("./model");
@@ -15,40 +16,40 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
   console.log("We are connected!");
 
-  app.post("/signup", (req, res) => {
-    const user1 = new userSchema({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-    user1.save(function(err, user) {
-      if (user != null) {
-        console.log("user successfully added", user);
-        res.status(200).send();
-      } else {
-        console.log("error 400");
-        res.status(400).send();
-      }
-    });
+  app.post("/signup", async (req, res) => {
+    try {
+      const user1 = new userSchema({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+      let saveUser = await user1.save();
+      console.log(saveUser);
+      res.status(200).send();
+    } catch {
+      console.log("already registered");
+      res.status(400).send();
+    }
   });
-  app.post("/login", (req, res) => {
-    const query = {
-      email: req.body.email,
-      password: req.body.password
-    };
-    userSchema.find(query, function(err, result) {
-      if (result.length === 0) {
-        console.log("wrong credential");
+  app.post("/login", async (req, res) => {
+    try {
+      const query = {
+        email: req.body.email,
+        password: req.body.password
+      };
+      let findUser = await userSchema.find(query);
+      if (findUser.length === 0) {
         res.status(404).send();
       } else {
         const objToSend = {
-          name: result[0].name,
-          email: result[0].email
+          name: findUser[0].name,
+          email: findUser[0].email
         };
-        console.log("I find user");
         res.status(200).send(JSON.stringify(objToSend));
       }
-    });
+    } catch {
+      res.sendStatus(500);
+    }
   });
 });
 
